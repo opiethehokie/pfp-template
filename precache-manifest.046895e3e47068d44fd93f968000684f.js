d0136cabad1b51a46cb939ebc51c0fa13f18ea54
Michael Peters <opiethehokie@gmail.com>
2021-10-11 08:51:26 -0400

    Updates

---
+++ precache-manifest.046895e3e47068d44fd93f968000684f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "887b6aae42b1d3eba8fa",
+    "url": "/smart-marketplace/static/css/main.58803189.chunk.css"
+  },
+  {
+    "revision": "887b6aae42b1d3eba8fa",
+    "url": "/smart-marketplace/static/js/main.887b6aae.chunk.js"
+  },
+  {
+    "revision": "a01fa0f908dd904f9a88",
+    "url": "/smart-marketplace/static/css/1.fe8d0f38.chunk.css"
+  },
+  {
+    "revision": "a01fa0f908dd904f9a88",
+    "url": "/smart-marketplace/static/js/1.a01fa0f9.chunk.js"
+  },
+  {
+    "revision": "7659d6500f439a1fcd28",
+    "url": "/smart-marketplace/static/js/runtime~main.7659d650.js"
+  },
+  {
+    "revision": "e02a58e0978b0df6bd7aba84eae5ae9d",
+    "url": "/smart-marketplace/index.html"
+  }
+];
